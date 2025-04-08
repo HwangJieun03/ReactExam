09f5f1f13ae9d7af7c2217b812f9eda957105dc9
jieun <jieunh0311@gmail.com>
2025-04-08 16:05:00 +0900

    0408-시계 추가 결과 코드 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
-{
-  /* 
-  작성자 : jieun
-  작성일 : 2025.4.7
-  내용 : 기능에 대한 내용
-  */
-}
+import TodoList from "./Todolist";
+import Clock from "./Timer";
 
 function App() {
-  let name = "리액트";
-
   return (
     <div className="container">
-      <h1 className="test">
-        Hello,
-        {name === "리액트" ? <h1>YES</h1> : null}!!
-      </h1>
-      <p>반갑습니다.</p>
+      <TodoList></TodoList>
+      <Clock></Clock>
     </div>
   );
 }
